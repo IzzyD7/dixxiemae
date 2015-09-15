d400e1da89368ed0ed5574d43e63fa76b4e2f6c8
Pete Isgrigg <sobchak7@gmail.com>
2015-09-15 15:44:25 -0400

    updated a social media icons and about, contact pages

---
+++ DixxieMae_Graphics/js/DM.js
@@ -0,0 +1,37 @@
+//Problem: User when clicking on image leads to a dead end.
+//Solution: create an overlay with the lage image. -lightbox.
+
+var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
+var $caption = $('<p></p>');
+
+//Add an image to overlay
+$overlay.append($image);
+
+//Add caption to overlay
+$overlay.append($caption);
+
+//Add an overlay
+$("body").append($overlay);
+
+//Capture the click event on a link to an image
+$("#imageGallery a").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+  //Update overlay with image linked in the link
+  $image.attr("src", imageLocation);
+  
+  //Show the overlay
+  $overlay.show();
+  
+  //Get child's alt attrib and set caption.
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+});
+
+//When overlay is clicked, 
+$overlay.click(function(){
+  //Hide the overlay. 
+  $overlay.hide();
+});
+  
